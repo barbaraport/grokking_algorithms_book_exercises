f5bc299e8ab7ac722d23ec4d279d41d678509102
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2025-05-21 20:08:19 -0300

    chapter 3: adds call stack example

---
+++ chapter_3/example_call_stack.py
@@ -0,0 +1,14 @@
+def greet(name):
+    print("Hi, " + name + "!")
+
+def greet2(name):
+    print("How are you, " + name + "?")
+
+def bye():
+    print("Okay. Bye bye!")
+
+name = "Maggie"
+
+greet(name)
+greet2(name)
+bye()
