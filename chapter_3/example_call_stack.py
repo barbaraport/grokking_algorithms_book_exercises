5ac29aa50db4a504a1ce74412563b6f560b93cc6
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2025-05-21 20:19:15 -0300

    chapter 3: fixes call stack example

--- chapter_3/example_call_stack.py
+++ chapter_3/example_call_stack.py
@@ -1,14 +1,18 @@
 def greet(name):
     print("Hi, " + name + "!")
+    greet2(name)
 
 def greet2(name):
     print("How are you, " + name + "?")
+    bye()
 
 def bye():
     print("Okay. Bye bye!")
 
 name = "Maggie"
 
+# it calls the greet2 inside of it
+# greet2 calls bye inside of it
+# when bye finishes, greet2 will finish
+# then greet's execution will be finished, after all functions inside of it finish.
 greet(name)
-greet2(name)
-bye()
